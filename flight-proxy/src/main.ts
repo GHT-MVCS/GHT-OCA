3d5b2ed5289dd8d33ded17d162df1b31bd65a712
x0George0x <jlmurcia@ucundinamarca.edu.co>
2026-05-01 21:28:19 +0000

    Descripción del cambio

--- flight-proxy/src/main.ts
+++ flight-proxy/src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
 
   app.enableCors({
@@ -12,12 +10,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: false,
     allowedHeaders: 'Content-Type, Accept, Authorization',
-  });
-
-  // ← MOVER estáticos a una ruta que no choque con /api
-  app.useStaticAssets(join(__dirname, '..', '..'), {
-    index: ['index.html'],
-    prefix: '/static',   // ← ESTE ES EL CAMBIO CLAVE
   });
 
   await app.listen(port);
